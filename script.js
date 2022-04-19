4347b3f579503a57ff9597cd1fda0d932b3873d5
Jorian Olk-Szost <jolkszost@gmail.com>
2022-04-18 20:33:20 -0400

    add loops to clear grid, generate new grid, style

--- script.js
+++ script.js
@@ -1,22 +1,72 @@
+let squareSides = 16;
+let containerHeight = 1000;
+let boxHeight = containerHeight / squareSides;
+let containerWidth = 1000;
+let boxWidth = containerWidth / squareSides;
 
-
-for (i = 0; i < 16; i++) {
+console.log(containerHeight, containerWidth, boxHeight, boxWidth)
+for (i = 0; i < (squareSides * squareSides); i++) {
     const box = document.createElement("div");
     box.setAttribute("class", "boxes");
-    document.getElementById("container").appendChild(box);
+    document.getElementById('container').appendChild(box);
+    
+
 };
-
-
 const boxes = document.querySelectorAll("div.boxes");
 
 
-console.log(boxes.length);
+
 
 
 boxes.forEach(function(b) {
      b.addEventListener("mouseover", function() {
     b.style.backgroundColor = "blue";
-})
+   
+}) 
+ b.style.width = boxWidth+'px';
+    b.style.height = boxHeight+'px';
 });
+
+document.getElementById('clearGrid').addEventListener('click', function(){
+                while (container.hasChildNodes()) {
+                container.removeChild(container.firstChild);
+            };
+       
+          let squareQ = prompt('how many squares per side you want?');
+          if (squareQ > 100) {
+              return alert('too many squares brah!');
+          } else if (squareQ < 4) {
+              return alert('too few squares brah!');
+          }  else {
+          for (ii = 0; ii < (squareQ * squareQ); ii++) {
+            const box = document.createElement("div");
+            box.setAttribute("class", "boxes");
+            document.getElementById("container").appendChild(box);
+            };
+            const boxes = document.querySelectorAll("div.boxes");
+           // boxes.forEach(function(b) {
+               
+           // 
+ 
+
+console.log(boxes.length);
+            boxes.forEach(function(b) {
+                b.addEventListener("mouseover", function() {
+               b.style.backgroundColor = "blue";
+              
+            
+                });
+             let newBoxHeight = containerHeight / squareQ;
+               let newBoxWidth = containerWidth / squareQ;
+                b.style.width = newBoxWidth+'px';
+                b.style.height = newBoxHeight+'px';});
+         
+        };
+      //    };
+
+
+});
+
+
 
     
