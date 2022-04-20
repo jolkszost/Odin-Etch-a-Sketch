4d71d6b3bb3a665460c882d4ef09ef14449d6cf5
Jorian Olk-Szost <jolkszost@gmail.com>
2022-04-20 13:18:55 -0400

    add randomRGB values, increasing alpha value.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let boxHeight = containerHeight / squareSides;
 let containerWidth = 1000;
 let boxWidth = containerWidth / squareSides;
 
+
 console.log(containerHeight, containerWidth, boxHeight, boxWidth)
 for (i = 0; i < (squareSides * squareSides); i++) {
     const box = document.createElement("div");
@@ -19,13 +20,31 @@ const boxes = document.querySelectorAll("div.boxes");
 
 
 boxes.forEach(function(b) {
-     b.addEventListener("mouseover", function() {
-    b.style.backgroundColor = "blue";
+    let alpha = .1;
+     b.addEventListener("mouseenter", function() {
+   // b.style.backgroundColor = "blue";
    
-}) 
+function getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max-min + 1) + min);
+};
+if (alpha < 1) {
+alpha += .1;
+};
+const randR = getRandom(0, 255);
+const randG = getRandom(0, 255);
+const randB = getRandom(0, 255);
+
+b.style.backgroundColor = `rgb(${randR}, ${randG}, ${randB}, ${alpha})`;
+
+
+ 
+});
  b.style.width = boxWidth+'px';
     b.style.height = boxHeight+'px';
 });
+
 
 document.getElementById('clearGrid').addEventListener('click', function(){
                 while (container.hasChildNodes()) {
@@ -44,29 +63,45 @@ document.getElementById('clearGrid').addEventListener('click', function(){
             document.getElementById("container").appendChild(box);
             };
             const boxes = document.querySelectorAll("div.boxes");
-           // boxes.forEach(function(b) {
-               
-           // 
+           
  
 
 console.log(boxes.length);
+
             boxes.forEach(function(b) {
-                b.addEventListener("mouseover", function() {
-               b.style.backgroundColor = "blue";
-              
+                let alpha = .1;
+                b.addEventListener("mouseenter", function() {
+            //   b.style.backgroundColor = "blue";
+            function getRandom(min, max) {
+                min = Math.ceil(min);
+                max = Math.floor(max);
+                return Math.floor(Math.random() * (max-min + 1) + min)
+            }
+             
+            if (alpha < 1) {
+                alpha += .1;
+                };   
+                 
+            const randR = getRandom(0, 255);
+            const randG = getRandom(0, 255);
+            const randB = getRandom(0, 255);
+         
+             
+                
+            
+
+            b.style.backgroundColor = `rgb(${randR}, ${randG}, ${randB}, ${alpha})`;
             
                 });
              let newBoxHeight = containerHeight / squareQ;
                let newBoxWidth = containerWidth / squareQ;
                 b.style.width = newBoxWidth+'px';
-                b.style.height = newBoxHeight+'px';});
-         
+                b.style.height = newBoxHeight+'px';
+            });
+                
+                
         };
-      //    };
+     
 
 
 });
-
-
-
-    
